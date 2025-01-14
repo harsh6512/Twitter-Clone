a735a370be858a63ca4a001cca688f9f350fc154
harsh6512 <harshkumar1987ynr@gmail.com>
2025-01-14 15:26:59 +0530

    resetup of frontend folder

--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getNotifications,
-	deleteNotifications, } from "../controllers/notification.controller.js";
+import {
+    getNotifications,
+    deleteNotifications,
+} from "../controllers/notification.controller.js";
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
