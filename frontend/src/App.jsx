4aa88d21447b991adac02e60df4681e7378b1538
harsh6512 <harshkumar1987ynr@gmail.com>
2025-01-15 22:16:53 +0530

    added the signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,13 @@ import './index.css';
 
 function App() {
   return (
-    <>
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
   );
 }
 
