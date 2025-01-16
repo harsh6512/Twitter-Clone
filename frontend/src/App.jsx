a22e586181337a7b4116fe42c4af68845895067e
harsh6512 <harshkumar1987ynr@gmail.com>
2025-01-16 13:42:42 +0530

    added the tailwind classes for some pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './index.css';
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from './pages/home/HomePage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
+import SideBar from './components/common/SideBar';
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+			<SideBar/>
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/' element={<HomePage/>} />
+				<Route path='/signup' element={<SignUpPage/>} />
 				<Route path='/login' element={<LoginPage />} />
 			</Routes>
 		</div>
