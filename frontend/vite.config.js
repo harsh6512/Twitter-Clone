1d8869629206a32bdd56bbef0d9c821ac95a6417
harsh6512 <harshkumar1987ynr@gmail.com>
2025-03-11 09:34:51 +0530

    production issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600, // Increase the limit (default is 500)
+    chunkSizeWarningLimit: 1600, 
   },
   server:{
     port:7000,proxy: {
